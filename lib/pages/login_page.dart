b45f106603308541862c8ab5f174309f4c486519
deryfj <80272513+deryfj@users.noreply.github.com>
2022-04-21 03:38:39 +0700

    Add files via upload
    Login, Register, Todo sudah digabungkan

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -100,7 +100,7 @@ class _LoginPageState extends State<LoginPage>{
 
 
                                    },
-                                 )
+                                 ),
 
                                ),
                                Container(
